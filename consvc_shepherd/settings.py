05a49b10a884086955df326d5dc9229da069eab0
tiftran <ttran@mozilla.com>
2022-03-17 13:08:28 -0700

    circle config to add tests and requirement changes

--- consvc_shepherd/settings.py
+++ consvc_shepherd/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = "consvc_shepherd.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASS"),
