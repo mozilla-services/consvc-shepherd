113cd6bf83393d91f7cb8907e5f49f24cf3be9e9
Tif Tran <ttran@mozilla.com>
2022-03-21 11:23:22 -0700

    feat: implement to_dict for partner, and advertiser models (#16)

--- consvc_shepherd/models/contile.py
+++ consvc_shepherd/models/contile.py
@@ -31,6 +31,18 @@ class Partner(models.Model):
                     f"{hostname} is not a valid hostname, hostnames should only contain alpha numeric characters and '.'"
                 )
 
+    def to_dict(self):
+        partner_dict = {
+            "DEFAULT": {
+                "click_hosts": list(self.click_hosts),
+                "impression_hosts": list(self.impression_hosts),
+            }
+        }
+        for advertiser in self.advertisers.all():
+            partner_dict.update(advertiser.to_dict())
+
+        return partner_dict
+
     def clean(self):
         super().clean()
         self.is_valid_host_list(self.click_hosts)
@@ -54,9 +66,29 @@ class Advertiser(models.Model):
         on_delete=models.CASCADE,
     )
 
-    # TODO
     def to_dict(self):
-        return {}
+        result = {}
+        geo_domain_combos = (
+            self.ad_urls.all()
+            .values_list("geo", "domain")
+            .distinct()
+            .order_by("geo", "domain")
+        )
+
+        for geo, domain in geo_domain_combos:
+            ad_urls = self.ad_urls.filter(geo=geo, domain=domain).order_by("path")
+            paths = [
+                {
+                    "value": ad_url.path,
+                    "matching": ad_url.get_matching_display(),
+                }
+                for ad_url in ad_urls
+            ]
+            if geo not in result:
+                result[geo] = []
+            result[geo].append({"host": domain, "paths": paths})
+
+        return {self.name: result}
 
     def __str__(self):
         return self.name
@@ -81,6 +113,5 @@ class AdvertiserUrl(models.Model):
     def clean(self) -> None:
         pass
 
-    # TODO
-    def to_dict(self) -> dict:
-        return {}
+    def __str__(self):
+        return f"{self.geo}{self.domain}{self.path}"
--- consvc_shepherd/tests/test_models_contile.py
+++ consvc_shepherd/tests/test_models_contile.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from consvc_shepherd.models import Partner
+from consvc_shepherd.models import Partner, Advertiser, AdvertiserUrl
 
 
 class TestPartnerModel(TestCase):
@@ -20,3 +20,85 @@ class TestPartnerModel(TestCase):
                 impression_hosts=["example.com", "test.example.com"],
                 click_hosts=["example.com", "ex@&!mple.com"],
             )
+
+    def test_to_dict_produces_correctly(self):
+        partner = Partner.objects.create(
+            name="Partner Advertiser",
+            impression_hosts=["example.com", "1.example.com"],
+            click_hosts=["2.example.com"],
+        )
+        advertiser1 = Advertiser.objects.create(name="Pocket", partner=partner)
+        advertiser2 = Advertiser.objects.create(name="Firefox", partner=partner)
+        AdvertiserUrl.objects.create(
+            advertiser=advertiser1,
+            path="/",
+            matching=False,
+            domain="example.com",
+            geo="Canada",
+        )
+        AdvertiserUrl.objects.create(
+            advertiser=advertiser1,
+            path="/hello",
+            matching=False,
+            domain="example.com",
+            geo="Canada",
+        )
+        AdvertiserUrl.objects.create(
+            advertiser=advertiser1,
+            path="/read",
+            matching=True,
+            domain="1.example.com",
+            geo="Canada",
+        )
+        AdvertiserUrl.objects.create(
+            advertiser=advertiser1,
+            path="/read",
+            matching=False,
+            domain="example.com",
+            geo="Germany",
+        )
+        AdvertiserUrl.objects.create(
+            advertiser=advertiser2,
+            path="/read",
+            matching=False,
+            domain="example.com",
+            geo="Germany",
+        )
+        self.maxDiff = None
+        expected_result = {
+            "DEFAULT": {
+                "click_hosts": ["2.example.com"],
+                "impression_hosts": ["example.com", "1.example.com"],
+            },
+            "Firefox": {
+                "DE": [
+                    {
+                        "host": "example.com",
+                        "paths": [{"matching": "prefix", "value": "/read"}],
+                    }
+                ]
+            },
+            "Pocket": {
+                "CA": [
+                    {
+                        "host": "1.example.com",
+                        "paths": [{"matching": "exact", "value": "/read"}],
+                    },
+                    {
+                        "host": "example.com",
+                        "paths": [
+                            {"matching": "prefix", "value": "/"},
+                            {"matching": "prefix", "value": "/hello"},
+                        ],
+                    },
+                ],
+                "DE": [
+                    {
+                        "host": "example.com",
+                        "paths": [{"matching": "prefix", "value": "/read"}],
+                    }
+                ],
+            },
+        }
+
+        self.assertEqual(partner.to_dict(), expected_result)
