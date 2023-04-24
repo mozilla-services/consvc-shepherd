6087765f0997eada8b74199a522385eb174367be
Taddes <tkorris@mozilla.com>
2023-04-24 10:06:46 -0400

    cleaner variable option

--- consvc_shepherd/storage.py
+++ consvc_shepherd/storage.py
@@ -5,19 +5,17 @@ from django.core.files.storage import default_storage
 from django.utils import timezone
 
 
-def send_to_storage(content, modified_file_name="") -> None:
+def send_to_storage(content, alt_file_name:str ="") -> None:
+    file_name: str = alt_file_name if alt_file_name else settings.GS_BUCKET_FILE_NAME
+
     if settings.DEBUG:
         logging.info(
-            f"Sending to storage, name:{settings.GS_BUCKET_FILE_NAME}, content: {content}"
+            f"Sending to storage, name:{file_name}, content: {content}"
         )
     else:
         current_time_string = timezone.now().strftime("%Y%m%d%H%M%S")
-        if modified_file_name:
-            latest_file_name = f"{settings.modified_file_name}_latest.json"
-            date_file_name = f"{settings.modified_file_name}_{current_time_string}.json"
-        else: 
-            latest_file_name =    f"{settings.GS_BUCKET_FILE_NAME}_latest.json"  
-            date_file_name = f"{settings.GS_BUCKET_FILE_NAME}_{current_time_string}.json"
+        latest_file_name =    f"{file_name}_latest.json"  
+        date_file_name = f"{file_name}_{current_time_string}.json"
 
         date_file = default_storage.open(date_file_name, "w")
         date_file.write(content)
