1491988eedd0c2caa1b5ab6f5e656a192a96bac2
Taddes <tkorris@mozilla.com>
2023-06-27 15:55:55 -0400

    initial comimt

--- contile/admin.py
+++ contile/admin.py
@@ -1,6 +1,8 @@
 """Admin module for consvc_shepherd/contile."""
 from django import forms
 from django.contrib import admin
+from django.shortcuts import render
+from django.urls import path
 
 from consvc_shepherd.utils import ShepherdMetrics
 from contile.models import Advertiser, AdvertiserUrl, Partner
@@ -34,6 +36,24 @@ class AdvertiserListAdmin(admin.ModelAdmin):
     model = Advertiser
     inlines = [AdUrlInline]
     ordering = ["name"]
+
+    def get_urls(self):
+        """Create new path to upload-csv"""
+        urls = super().get_urls()
+        new_urls = [path("upload-csv/", self.upload_csv)]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        """Render function for new csv endpoint."""
+        return render(request, "admin/csv_upload.html")
+
+        # if request.method == "POST":
+        #     csv_file = request.FILES["csv_upload"]
+        # if not csv_file.name.endswith('.csv'):
+        #     messages.warning(request, 'The wrong file type was uploaded')
+        #     return HttpResponseRedirect(request.path_info)
+        # file_data = csv_file.read().decode("utf-8")
+        # csv_data = file_data.split("\n")
 
 
 @admin.register(Partner)
