76c8c19a092c0f8ecc781a8f978930f0cff746f6
Masha Lifshin <mlifshin@pm.me>
2024-07-29 16:39:20 -0700

    Update sync_boostr_data.py
    Co-authored-by: Can Berk Güder <cbguder@mozilla.com>

--- sync_boostr_data.py
+++ sync_boostr_data.py
@@ -12,7 +12,7 @@ django.setup()
 from consvc_shepherd.models import BoostrDeal, BoostrDealProduct, BoostrProduct
 
 @dataclass(frozen=True)
-class BoostrSyncConfig():
+class BoostrSyncConfig:
     """Object to wrap up various environment configuration for the boostr snyc script"""
     base_url: str
     headers: dict[str, str]
