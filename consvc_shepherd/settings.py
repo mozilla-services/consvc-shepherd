62c6d11255189ca036f10857f514f74f03bc6ed1
Kauana Santos <ksantos@mozilla.com>
2024-09-15 11:52:52 -0700

    Fixes DEFAULT_FILE_STORAGE deprecation warning
    - Django 4.2 changed how storage objects are configured so its easier to configure multiple storage objects. As a result, DEFAULT_FILE_STORAGE has been deprecated in favor of STORAGES. DEFAULT_FILE_STORAGE will be removed on Django 5.1.
    - For more info check [Google Cloud Storage Configuration](https://django-storages.readthedocs.io/en/latest/backends/gcloud.html#configuration-settings)

--- consvc_shepherd/settings.py
+++ consvc_shepherd/settings.py
@@ -158,8 +158,18 @@ STATIC_URL: str = "static/"
 
 DEFAULT_AUTO_FIELD: str = "django.db.models.BigAutoField"
 
-DEFAULT_FILE_STORAGE: str = "storages.backends.gcloud.GoogleCloudStorage"
-GS_BUCKET_NAME = env("GS_BUCKET_NAME", default="")
+STORAGES: dict[str, Any] = {
+    "default": {
+        "BACKEND": "storages.backends.gcloud.GoogleCloudStorage",
+        "OPTIONS": {
+            "bucket_name": env("GS_BUCKET_NAME", default=""),
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 GS_BUCKET_FILE_NAME = env("GS_BUCKET_FILE_NAME", default="settings_from_shepherd")
 ALLOCATION_FILE_NAME: str = env("ALLOCATION_FILE_NAME", default="allocation_file")
 
