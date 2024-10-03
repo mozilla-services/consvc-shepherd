a631197233bbcc03d4cd4cf20cb25089c7652956
Kauana Santos <ksantos@mozilla.com>
2024-10-02 19:28:24 -0700

    [SHEP-17] Add BQ data fetcher script (#264)
    ## References
    
    [SHEP-17](https://mozilla-hub.atlassian.net/browse/SHEP-17)
    
    ## Problem Statement
    
    We want to be able to pull some Kevel metadata from BigQuery to display
    in on the Shepherd UI. This script retrieves selected ad data from
    BigQuery and inserts it into Shepherd DB. Once configured, this script
    will run daily, fetching some Kevel metadata for flights.
    
    ## Proposed Changes
    
    - **Add the "sync_bq_data" script**: This script will use the BigQuery
    client to query Kevel metadata in BigQuery. If the data is successfully
    fetched, it will be inserted into the Shepherd database. If the script
    runs again, it will update the click and impression data for flights
    rather than creating new rows in the db.
    - **Add unit tests for the sync_bq_data script**: Ensure that the
    functions in the script are behaving correctly.
    
    ## Verification Steps
    
    **Note**: This script includes a field (`flight_name`) that is not yet
    available in BigQuery. As a result, running the script as is will cause
    an error because the BigQuery client won't find the flight name. I’m
    coordinating with Curtis to get this field added, but in the meantime,
    to test the script, we can comment out or temporarily delete the
    `flight_name` field from the queries in this script.
    
    To manually run and test the script:
    
    1. Start Shepherd with `docker compose up --build`.
    2. Exec into the Shepherd container with `make debug`.
    3. Pass your gcloud credentials to the shepherd container to access the
    BigQuery view
    `moz-fx-data-shared-prod.ads.consolidated_ad_metrics_daily_pt`:
        1. Login to gcloud locally `gcloud auth application-default login`.
        2. Copy your gcloud creds into the shepherd container:
       ```bash
    docker cp ~/.config/gcloud/application_default_credentials.json
    consvc-shepherd-app-1:/app/application_default_credentials.json
       
    export
    GOOGLE_APPLICATION_CREDENTIALS="/app/application_default_credentials.json"
    
       ```
    4. Run the script. Note that date is optional and defaults to today's
    date.
        ```bash
    python manage.py sync_bq_data --project_id "moz-fx-ads-nonprod" --date
    $(date +%F)
        ```
        We are using the `moz-fx-ads-nonprod` project ID to test this.
        
    If successful you should see: `BigQuery fetch completed for data
    <today's date>`.
    5. Now the `consvc_shepherd_deliveredflight` db table was populated with
    click and impression data. You can verify this by checking the table in
    PostgreSQL (exec into `db-1` container), or by viewing the Delivered
    flights tab in the admin interface
    
    6. Last but no least, run the tests and ensure they pass.
    
    
    [SHEP-17]:
    https://mozilla-hub.atlassian.net/browse/SHEP-17?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ consvc_shepherd/management/commands/sync_bq_data.py
@@ -0,0 +1,162 @@
+"""Django admin custom command for fetching ad data from BigQuery and saving it to Shepherd DB"""
+
+import logging
+import traceback
+from datetime import datetime
+
+import pandas
+from django.core.management import CommandError
+from django.core.management.base import BaseCommand
+from google.cloud import bigquery
+
+from consvc_shepherd.models import DeliveredFlight
+
+
+class Command(BaseCommand):
+    """Django admin custom command for fetching ad data from BigQuery and saving it to Shepherd DB"""
+
+    help = "Run a script that fetches ad data from BigQuery and stores it in Shepherd"
+
+    def add_arguments(self, parser):
+        """Register expected command line arguments"""
+        parser.add_argument(
+            "--project_id",
+            type=str,
+            help="The GCP project ID that will interact with BQ, e.g. moz-fx-ads-nonprod.",
+        )
+        parser.add_argument(
+            "--date",
+            default=datetime.today().strftime("%Y-%m-%d"),
+            type=str,
+            help="The date we want to capture metrics for, e.g. 2024-09-18. By default, it will use today's date.",
+        )
+
+    def handle(self, *args, **options):
+        """Handle running the command"""
+        self.stdout.write(f"Starting BigQuery sync for date {options['date']}")
+
+        try:
+            datetime.strptime(options["date"], "%Y-%m-%d")
+        except ValueError:
+            raise CommandError("Invalid date format. Please use YYYY-MM-DD")
+
+        try:
+            bigquery.Client(project=options["project_id"])
+        except Exception as e:
+            raise CommandError(
+                f"Invalid project ID: {options['project_id']}. Error: {e}"
+            )
+
+        syncer = BQSyncer(options["project_id"], options["date"])
+
+        try:
+            syncer.sync_data()
+        except Exception as e:
+            raise CommandError(f"An error occurred: {e}")
+
+        self.stdout.write(f"BigQuery sync completed for date {options['date']}")
+
+
+class BQSyncer:
+    """Wrap up interaction with BigQuery"""
+
+    log: logging.Logger
+    project_id: str
+    date: str
+
+    def __init__(self, project_id: str, date: str):
+        self.log = logging.getLogger("sync_bigquery_ads_data")
+        self.project_id = project_id
+        self.date = date
+
+    def query_bq(self) -> pandas.DataFrame:
+        """Create SQL query, send query BQ through its client"""
+        query = """
+            SELECT
+                submission_date,
+                campaign_id,
+                campaign_name,
+                flight_id,
+                provider,
+                SUM(clicks) AS clicks,
+                SUM(impressions) AS impressions
+            FROM
+                `moz-fx-data-shared-prod.ads.consolidated_ad_metrics_daily_pt`
+            WHERE
+                submission_date = @submission_date
+                AND flight_id IS NOT NULL
+                AND campaign_id IS NOT NULL
+            GROUP BY
+                submission_date,
+                campaign_id,
+                campaign_name,
+                flight_id,
+                provider
+        """
+
+        client = bigquery.Client(project=self.project_id)
+
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("submission_date", "DATE", self.date)
+            ]
+        )
+
+        try:
+            query_job = client.query(query, job_config=job_config)
+            results = query_job.result()
+
+            if results.total_rows == 0:
+                self.log.warning(f"No data returned for the date {self.date}")
+                return pandas.DataFrame()
+
+            df = results.to_dataframe()
+
+            self.log.info(f"BQ data pulled successfully for date {self.date}")
+            return df
+        except Exception as e:
+            self.log.error(f"An error occurred while querying BigQuery: {e}")
+            raise  # Re-raise the exception to propagate it further
+
+    def upsert_data(self, df):
+        """Upsert data queried from BigQuery into Shepherd DB"""
+        for _, row in df.iterrows():
+            submission_date = row["submission_date"]
+            campaign_id = row["campaign_id"]
+            campaign_name = row["campaign_name"]
+            flight_id = row["flight_id"]
+            provider = row["provider"]
+            clicks = row["clicks"]
+            impressions = row["impressions"]
+
+            delivered_flight, created = DeliveredFlight.objects.update_or_create(
+                submission_date=submission_date,
+                campaign_id=campaign_id,
+                campaign_name=campaign_name,
+                flight_id=flight_id,
+                provider=provider,
+                # If script runs again in the same day, just update the clicks and impressions
+                defaults={
+                    "clicks_delivered": clicks,
+                    "impressions_delivered": impressions,
+                },
+            )
+
+            if created:
+                self.log.info(f"Created new DeliveredFlight: {delivered_flight}")
+            else:
+                self.log.info(f"Updated DeliveredFlight: {delivered_flight}")
+
+    def sync_data(self):
+        """BQ Syncer entrypoint"""
+        try:
+            df = self.query_bq()
+
+            if not df.empty:
+                self.upsert_data(df)
+
+            self.log.info("BigQuery sync process has completed successfully")
+        except Exception as e:
+            error = f"Exception: {str(e):} Trace: {traceback.format_exc()}"
+            self.log.error(f"BigQuery sync process has encounterd an error: {error}")
+            raise
