4348af4cf386195f2d8b3c91500d7d6ec684cdb8
Nana Gilbert-Baffoe <ngilbertbaffoe@mozilla.com>
2024-10-07 17:12:51 -0700

    New InventoryOverview route

--- ad-ops-dashboard/src/routes.tsx
+++ ad-ops-dashboard/src/routes.tsx
@@ -1,5 +1,6 @@
 import Dashboard from "./pages/Dashboard/Dashboard";
 import CampaignOverview from "./pages/CampaignOverview/CampaignOverview";
+import InventoryOverview from "./pages/InventoryOverview/InventoryOverview";
 import Page404 from "./pages/Page404";
 import Layout from "./components/Layout/AppLayout";
 
@@ -10,6 +11,7 @@ export const routes = [
     children: [
       { index: true, element: <Dashboard /> },
       { path: "campaign", element: <CampaignOverview /> },
+      { path: "inventory", element: <InventoryOverview /> },
       { path: "*", element: <Page404 /> },
     ],
   },
