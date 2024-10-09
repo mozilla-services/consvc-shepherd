e2b441656dcbabae8b29e23ec183dc5cee0c3c38
Ayesha Khan <akhan@mozilla.com>
2024-10-09 14:26:31 -0700

    [SHEP-109] Fixed Linting Issues and Enabled Linting in Makefile (#275)
    ## References
    
    [SHEP-123](https://mozilla-hub.atlassian.net/browse/SHEP-123)
    
    ## Problem Statement
    
    Lint issues need to be fixed for the campaigns pages and components
    since the lint setup has been completed and merged.
    
    ## Proposed Changes
    
    1. Added lint fixes for the campaigns pages and their components
    2. Enabled ESLint in the `make lint` command
    
    ## Verification Steps
    
    run `make lint` command 
    
    
    [SHEP-123]:
    https://mozilla-hub.atlassian.net/browse/SHEP-123?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- ad-ops-dashboard/src/data/campaigns.ts
+++ ad-ops-dashboard/src/data/campaigns.ts
@@ -6,15 +6,18 @@ import {
   CampaignFormSchema,
   SplitFormSchema,
 } from "../utils/schemas/campaignFormSchema";
-import { ErrorResponse } from "../types";
+import { ErrorResponse, Campaign } from "../types";
+
+function fetchCampaign(): Promise<Campaign[]> {
+  return axios
+    .get<Campaign[]>(apiRoutes.campaigns)
+    .then((response) => response.data);
+}
 
 export const useGetCampaignsQuery = () => {
   const getCampaigns = useQuery({
     queryKey: ["campaigns"],
-    queryFn: async () => {
-      const { data } = await axios.get(apiRoutes.campaigns);
-      return data;
-    },
+    queryFn: fetchCampaign,
   });
 
   return getCampaigns;
@@ -43,9 +46,9 @@ export const useSplitCampaignMutation = () => {
       }
       toast.error(errorMessage);
     },
-    onSuccess: () => {
+    onSuccess: async () => {
       toast.success("Campaign splitted successfully.");
-      queryClient.invalidateQueries({ queryKey: ["campaigns"] });
+      await queryClient.invalidateQueries({ queryKey: ["campaigns"] });
     },
   });
 };
@@ -60,18 +63,24 @@ export const useCreateCampaignMutation = () => {
     onMutate: (data) => {
       return data;
     },
-    onError: (error: any) => {
-      if (error.response?.data) {
-        const errorMessage =
-          error.response.data.non_field_errors?.[0] || "An error occurred";
-        toast.error(errorMessage);
+    onError: (error: AxiosError<ErrorResponse>) => {
+      const nonFieldErrors = error.response?.data.non_field_errors;
+
+      let errorMessage: string;
+      if (
+        nonFieldErrors &&
+        Array.isArray(nonFieldErrors) &&
+        nonFieldErrors.length > 0
+      ) {
+        errorMessage = nonFieldErrors[0];
       } else {
-        toast.error("An unexpected error occurred");
+        errorMessage = "An unexpected error occurred";
       }
+      toast.error(errorMessage);
     },
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       toast.success("Campaign added successfully.");
-      queryClient.invalidateQueries({ queryKey: ["campaigns"] });
+      await queryClient.invalidateQueries({ queryKey: ["campaigns"] });
       return data;
     },
   });
@@ -87,18 +96,25 @@ export const useUpdateCampaignMutation = (id: number | undefined) => {
     onMutate: (data) => {
       return data;
     },
-    onError: (error: any) => {
-      if (error.response?.data) {
-        const errorMessage =
-          error.response.data.non_field_errors?.[0] || "An error occurred";
-        toast.error(errorMessage);
+    onError: (error: AxiosError<ErrorResponse>) => {
+      const nonFieldErrors = error.response?.data.non_field_errors;
+
+      let errorMessage: string;
+
+      if (
+        nonFieldErrors &&
+        Array.isArray(nonFieldErrors) &&
+        nonFieldErrors.length > 0
+      ) {
+        errorMessage = nonFieldErrors[0];
       } else {
-        toast.error("An unexpected error occurred");
+        errorMessage = "An unexpected error occurred";
       }
+      toast.error(errorMessage);
     },
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       toast.success("Campaign updated successfully.");
-      queryClient.invalidateQueries({ queryKey: ["campaigns"] });
+      await queryClient.invalidateQueries({ queryKey: ["campaigns"] });
       return data;
     },
   });
@@ -117,8 +133,8 @@ export const useDeleteCampaignMutation = () => {
     onError: (error) => {
       toast.error(error.message);
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["campaigns"] });
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["campaigns"] });
       toast.success("Campaign deleted successfully.");
     },
   });
