cdb232f4ff3a5b0237edba4363e3b7a43fd5398b
Luc Lisi <luc.lisi94@gmail.com>
2024-10-22 16:36:14 -0700

    [SHEP-123] Locked debug pannel behind `npm run dev` mode only

--- ad-ops-dashboard/src/pages/Debug/Debug.tsx
+++ ad-ops-dashboard/src/pages/Debug/Debug.tsx
@@ -1,6 +1,7 @@
 
 import * as Sentry from "@sentry/react";
 import { useEffect, useState } from "react";
+import Page404 from "../Page404";
 export default function Debug() {
 
   const [isPageBroken, setIsPageBroken] = useState(false)
@@ -22,24 +23,33 @@ export default function Debug() {
     }
   }
 
-  return (
-    <div style={{ textAlign: "center", padding: 20 }}>    <h1> Debug pannel </h1>
-      <div style={{ display: "flex", gap: 20, justifyContent: "center" }}>
-        <button
-          type="button"
-          onClick={makeError}> Throw caught sentry error
-        </button>
-        <button
-          type="button"
-          onClick={() => { setErrorCount(1); }}> Break useEffect
-        </button>
-        <button
-          type="button"
-          onClick={() => { setIsPageBroken(true); }}> Break Page
-        </button>
+  if (process.env.NODE_ENV === "development") {
+    return (
+      <div style={{ textAlign: "center", padding: 20 }}>
+        <div>
+          <h2> Debug pannel! (Dev Env Only) </h2>
+          <p>Include any buttons here that force certain actions or frontend states. This page is only visible running `npm run dev` and cannot be viewed in production builds</p>
+        </div>
+        <div style={{ display: "flex", paddingTop: 20, gap: 20, justifyContent: "center" }}>
+          <button
+            type="button"
+            onClick={makeError}> Throw caught sentry error
+          </button>
+          <button
+            type="button"
+            onClick={() => { setErrorCount(1); }}> Break useEffect
+          </button>
+          <button
+            type="button"
+            onClick={() => { setIsPageBroken(true); }}> Break Page
+          </button>
+        </div>
+        { /* eslint-disable-next-line */}
+        <div> {isPageBroken && <ErrorComponent />} </div>
       </div>
-      { /* eslint-disable-next-line */}
-      <div> {isPageBroken && <ErrorComponent />} </div>
-    </div>
-  )
+    )
+  } else {
+    return < Page404 />
+  }
+
 } 
