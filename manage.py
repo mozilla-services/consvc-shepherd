9dd99a42cd93ba1f40a550202179a1c01fa4243b
Luc Lisi <luc.lisi94@gmail.com>
2024-10-21 22:13:31 -0700

    [SHEP-36] Removing old human readable timestamps in manage.py. Logic moved to settings.py

--- manage.py
+++ manage.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-from argparse import OPTIONAL
-import os
 import sys
-import logging
-import environ
-
-from pathlib import Path
-
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "consvc_shepherd.settings")
-    env = environ.Env(APP_ENV=(str, OPTIONAL))
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    environ.Env.read_env(BASE_DIR / ".env")
-    
-    # For local dev, human readable timestamps are added to help with debugging.
-    if env("APP_ENV") == "dev":
-        logging.basicConfig(format="%(asctime)s.%(msecs)03d", level=logging.INFO, datefmt="%Y-%m-%d,%H:%M:%S")
 
     try:
         from django.core.management import execute_from_command_line
